28fc5e6ee384a8b3c260f760c9a423c9b69d3dfc
yang <everymoment.studio@gmail.com>
2017-06-07 15:17:29 +0800

    fixed echo exec result

--- deploy-dian.php
+++ deploy-dian.php
@@ -2,7 +2,7 @@
 $path = "/var/www/html/dian";
 $a='';
 chdir($path);
-exec("sudo git fetch --all");
-exec("sudo git reset --hard origin/master");
+echo exec("sudo git fetch --all");
+echo exec("sudo git reset --hard origin/master");
 echo "<pre> Succesfully fetch all the files.</pre>";
 ?>
